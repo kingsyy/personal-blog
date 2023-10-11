b0046b5841fbd8b298a49cb482ba9f54499d4e63
arthur <mauistuff@hotmail.com>
2023-10-11 17:54:04 +0200

    add basic image cloudflare loader

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const {withContentlayer} = require("next-contentlayer")
 
 const nextConfig = {
     output: 'export',
+    images: {
+      loader: 'custom',
+      loaderFile: './src/components/image-loader.js'
+    },
     compiler:{
         removeConsole: true,
     }
---
+++ src/components/image-loader.js
@@ -0,0 +1,7 @@
+export default function imageLoader({
+    src,
+    width,
+    quality,
+  }) {
+    return `https://02902020.personal-blog-2e5.pages.dev/${src}`
+  }
