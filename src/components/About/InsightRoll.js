e153f24369f20edd1fa0ede6f7e8caf70cd1ef95
arthur <mauistuff@hotmail.com>
2023-10-15 14:09:41 +0200

    fix | on ending

--- src/components/About/InsightRoll.js
+++ src/components/About/InsightRoll.js
@@ -4,9 +4,9 @@ const InsightRoll = ({ insights }) => {
   return (
     <div className="w-full bg-accent dark:bg-accentDark text-light dark:text-dark whitespace-nowrap overflow-hidden">
       <div className="animate-roll  w-full py-2 sm:py-3 flex items-center justify-center capitalize font-semibold tracking-wider text-sm sm:text-base">
-        {insights.map((text) => (
+        {insights.map((text, i, insights) => (
           <div>
-            {text} <span className="px-4">|</span>{" "}
+            {text} <span className="px-4">{insights.length - 1 === i ? "" : "|"}</span>{" "}
           </div>
         ))}
       </div>
