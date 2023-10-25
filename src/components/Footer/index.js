03d51edf46d5c4792de1d0826e5689f90e94758f
arthur <mauistuff@hotmail.com>
2023-10-25 20:29:06 +0200

    Add privacy terms

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,14 +2,16 @@
 import React from "react";
 import { DribbbleIcon, GithubIcon, LinkedinIcon, TwitterIcon } from "../Icons";
 import siteMetadata from "@/src/utils/siteMetaData";
+import Link from "next/link";
 
 const Footer = () => {
   return (
     <footer className="relative mt-auto rounded-2xl bg-dark dark:bg-accentDark/90 flex items-center text-light dark:text-dark w-full  font-medium py-6 px-8 flex-col md:flex-row justify-between">
-        <span className="text-center">
-          &copy;2023 Arthur. All rights reserved.
-        </span>
-        <div className="flex items-center">
+      <span className="text-center">
+        &copy;2023 Arthur. All rights reserved.
+        <Link href="/privacy"> Privacy & Terms</Link>
+      </span>
+      <div className="flex items-center">
         <a
           href={siteMetadata.linkedin}
           className="inline-block w-6 h-6 mr-4"
@@ -43,12 +45,12 @@ const Footer = () => {
           <DribbbleIcon className="hover:scale-125 transition-all ease duration-200" />
         </a>
       </div>
-        <div className="text-center">
-          Made with &hearts; by{" "}
-          <a href="https://devdreaming.com" className="underline" target="_blank">
-            Arthur
-          </a>
-        </div>
+      <div className="text-center">
+        Made with &hearts; by{" "}
+        <a href="/about" className="underline">
+          Arthur
+        </a>
+      </div>
     </footer>
   );
 };
