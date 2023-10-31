089c8a282a8ddd5687d03d823632e29c51114fdc
arthur <mauistuff@hotmail.com>
2023-10-31 18:09:51 +0100

    privacy to center remove icons

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,7 +1,5 @@
 "use client";
 import React from "react";
-import { DribbbleIcon, GithubIcon, LinkedinIcon, TwitterIcon } from "../Icons";
-import siteMetadata from "@/src/utils/siteMetaData";
 import Link from "next/link";
 
 const Footer = () => {
@@ -9,42 +7,8 @@ const Footer = () => {
     <footer className="relative mt-auto rounded-2xl bg-dark dark:bg-accentDark/90 flex items-center text-light dark:text-dark w-full  font-medium py-6 px-8 flex-col md:flex-row justify-between">
       <span className="text-center">
         &copy;2023 Arthur. All rights reserved.
-        <Link href="/privacy"> Privacy & Terms</Link>
       </span>
-      <div className="flex items-center">
-        <a
-          href={siteMetadata.linkedin}
-          className="inline-block w-6 h-6 mr-4"
-          aria-label="Reach out to me via LinkedIn"
-          target="_blank"
-        >
-          <LinkedinIcon className="hover:scale-125 transition-all ease duration-200" />
-        </a>
-        <a
-          href={siteMetadata.twitter}
-          className="inline-block w-6 h-6 mr-4"
-          aria-label="Reach out to me via Twitter"
-          target="_blank"
-        >
-          <TwitterIcon className="hover:scale-125 transition-all ease duration-200" />
-        </a>
-        <a
-          href={siteMetadata.github}
-          className="inline-block w-6 h-6 mr-4 fill-light"
-          aria-label="Check my profile on Github"
-          target="_blank"
-        >
-          <GithubIcon className="fill-light dark:fill-dark  hover:scale-125 transition-all ease duration-200" />
-        </a>
-        <a
-          href={siteMetadata.dribbble}
-          className="inline-block w-6 h-6 mr-4"
-          aria-label="Check my profile on Dribbble"
-          target="_blank"
-        >
-          <DribbbleIcon className="hover:scale-125 transition-all ease duration-200" />
-        </a>
-      </div>
+      <Link href="/privacy"> Privacy & Terms</Link>
       <div className="text-center">
         Made with &hearts; by{" "}
         <a href="/about" className="underline">
