22f591990951479bd7c3ca30f3428890bdf48e86
arthur <mauistuff@hotmail.com>
2024-01-13 18:52:57 +0100

    fix not all categories were shown

--- src/app/categories/[slug]/page.js
+++ src/app/categories/[slug]/page.js
@@ -1,6 +1,7 @@
 import { allBlogs } from "@/.contentlayer/generated";
 import BlogLayoutThree from "@/src/components/Blog/BlogLayoutThree";
 import Categories from "@/src/components/Blog/Categories";
+import { et } from "date-fns/locale";
 import GithubSlugger, { slug } from "github-slugger";
 
 const slugger = new GithubSlugger();
@@ -27,30 +28,33 @@ export async function generateStaticParams() {
 export async function generateMetadata({ params }) {
   return {
     title: `${params.slug.replaceAll("-"," ")} Blogs`,
-    description: `Learn more about ${params.slug === "all" ? "web development" : params.slug} through our collection of expert blogs and tutorials`,
+    description: `Learn more about ${params.slug === "all" ? "web development" : params.slug} through our collection of blogs and tutorials`,
   };
 }
 
+function getAllCategories() {
+  const categories = ["all"];
+  allBlogs.forEach(blog => blog.tags.forEach(tag => {
+    const slugified = slug(tag);
+    if (!categories.includes(slugified)){
+      categories.push(slugified)
+    }
+  }));
+  return categories.sort();
+}
+
+function getBlogs(category) {
+  return category === "all" ? allBlogs :
+  allBlogs.filter((blog) => blog.tags.some(tag => slug(tag) === category));
+}
 
 const CategoryPage = ({ params }) => {
-  const allCategories = ["all"];
-  const blogs = allBlogs.filter((blog) => {
-    return blog.tags.some((tag) => {
-      const slugified = slug(tag);
-      if (!allCategories.includes(slugified)) {
-        allCategories.push(slugified);
-      }
-      if (params.slug === "all") {
-        return true;
-      }
-      return slugified === params.slug;
-    });
-  });
-
+  const allCategories = getAllCategories();
+  const blogs = getBlogs(params.slug);
   return (
     <article className="mt-12 flex flex-col text-dark dark:text-light">
       <div className=" px-5 sm:px-10  md:px-24  sxl:px-32 flex flex-col">
-        <h1 className="mt-6 font-semibold text-2xl md:text-4xl lg:text-5xl">#{params.slug}</h1>
+        <h1 className="mt-6 font-semibold text-2xl md:text-4xl lg:text-5xl capitalize" >#{params.slug}</h1>
         <span className="mt-2 inline-block">
           Discover more categories and expand your knowledge!
         </span>
@@ -69,3 +73,41 @@ const CategoryPage = ({ params }) => {
 };
 
 export default CategoryPage;
+
+
+// function getAllCategories() {
+//   const categories = ["all"];
+
+//   return categories.d;
+// }
+
+// function getBlogs(category) {
+//   return category === "all" ? allBlogs :
+//   allBlogs.filter((blog) => {
+//     return blog.tags.some((tag) => {
+//       const slugified = slug(tag);
+//       // allCategories.push(slugified);
+//       // if (!allCategories.includes(slugified)) {
+//       //   allCategories.push(slugified);
+//       // }
+//       // if (params.slug === "all") {
+//       //   return true;
+//       // }
+//       return slugified === params.slug;
+//     });
+//   });
+
+// }
+
+// function getAllCategories() {
+//   return ["all"];
+// }
+
+// function getBlogs(category) {
+//   return category === "all" ? allBlogs :
+//   allBlogs.filter((blog) => blog.tags.some(tag => slug(tag) === category));
+// }
+
+// const CategoryPage = ({ params }) => {
+//   const allCategories = getAllCategories();
+//   const blogs = getBlogs(params.slug);
