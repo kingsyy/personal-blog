a259d22eee00ed911e39bb27d88e9ad6e02785b9
Arthur <kingsy@pm.me>
2024-10-12 14:34:14 +0200

    small text fixes

--- src/app/(about)/layout.tsx
+++ src/app/(about)/layout.tsx
@@ -1,17 +1,16 @@
 import InsightRoll from "@/components/About/InsightRoll";
 const insights = [
-    "6+ Years of experience in C#",
-    "Software development associate's degree",
     "Fullstack developer",
+    "6+ Years of experience in C# and web",
+    "Software development degree",
     "Inclusive user minded designs",
-    "Privacy and security aware",
+    "Privacy and security first",
     "From idea to fully hosted solutions",
-    "Always happy to help"
   ];
 
 export default function AboutLayout({ children } : any) {
   return (
-    <main className="w-full flex flex-col items-center justify-between">
+    <main className="pt-16 md:pt-0 w-full flex flex-col items-center justify-between">
       <InsightRoll texts={insights} />
       {children}
     </main>
